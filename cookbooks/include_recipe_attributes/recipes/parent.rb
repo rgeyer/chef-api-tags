d6d2fb741fd6e3e1bc4f2c35a8e88fed10d6c30b
rgeyer <me@ryangeyer.com>
2011-01-21 13:43:59 -0800

    Lets not access the vars before we pass them to the child

--- cookbooks/include_recipe_attributes/recipes/parent.rb
+++ cookbooks/include_recipe_attributes/recipes/parent.rb
@@ -1,12 +1,8 @@
 Chef::Log.info("From parent recipe (chef compile), include_recipe_attributes/parent_only_1 == #{node[:include_recipe_attributes][:parent_only_1]}")
-Chef::Log.info("From parent recipe (chef compile), include_recipe_attributes/parent_and_child_1 == #{node[:include_recipe_attributes][:parent_and_child_1]}")
-Chef::Log.info("From parent recipe (chef compile), include_recipe_attributes/parent_and_child_2 == #{node[:include_recipe_attributes][:parent_and_child_2]}")
 
 ruby_block "In a chef resource" do
   block do
     Chef::Log.info("From parent recipe (chef resource), include_recipe_attributes/parent_only_1 == #{node[:include_recipe_attributes][:parent_only_1]}")
-    Chef::Log.info("From parent recipe (chef resource), include_recipe_attributes/parent_and_child_1 == #{node[:include_recipe_attributes][:parent_and_child_1]}")
-    Chef::Log.info("From parent recipe (chef resource), include_recipe_attributes/parent_and_child_2 == #{node[:include_recipe_attributes][:parent_and_child_2]}")
   end
 end
 
