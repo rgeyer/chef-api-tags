49517709fbbcdc761dd3e9085f0b1ad6ed7e72f4
rgeyer <me@ryangeyer.com>
2011-01-21 13:24:17 -0800

    Accessing the node attributes in chef compile and a chef resource

--- cookbooks/include_recipe_attributes/recipes/child.rb
+++ cookbooks/include_recipe_attributes/recipes/child.rb
@@ -1,2 +1,9 @@
-Chef::Log.info("From child recipe, include_recipe_attributes/parent_and_child_1 == #{node[:include_recipe_attributes][:parent_and_child_1]}")
-Chef::Log.info("From child recipe, include_recipe_attributes/parent_and_child_2 == #{node[:include_recipe_attributes][:parent_and_child_2]}")
+Chef::Log.info("From child recipe (chef compile), include_recipe_attributes/parent_and_child_1 == #{node[:include_recipe_attributes][:parent_and_child_1]}")
+Chef::Log.info("From child recipe (chef compile), include_recipe_attributes/parent_and_child_2 == #{node[:include_recipe_attributes][:parent_and_child_2]}")
+
+ruby_block "In a chef resource" do
+  block do
+    Chef::Log.info("From child recipe (chef resource), include_recipe_attributes/parent_and_child_1 == #{node[:include_recipe_attributes][:parent_and_child_1]}")
+    Chef::Log.info("From child recipe (chef resource), include_recipe_attributes/parent_and_child_2 == #{node[:include_recipe_attributes][:parent_and_child_2]}")
+  end
+end
